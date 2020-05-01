369f098cefdfaf4d085b2816067304c6f4b793d0
Zach Daniel <zachary.s.daniel@gmail.com>
2020-05-01 12:32:56 -0400

    init

---
+++ lib/ash_graphql.ex
@@ -0,0 +1,18 @@
+defmodule AshGraphql do
+  @moduledoc """
+  Documentation for `AshGraphql`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AshGraphql.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ash_graphql_test.exs
@@ -0,0 +1,8 @@
+defmodule AshGraphqlTest do
+  use ExUnit.Case
+  doctest AshGraphql
+
+  test "greets the world" do
+    assert AshGraphql.hello() == :world
+  end
+end
