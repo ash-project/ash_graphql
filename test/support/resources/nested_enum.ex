04de3decad37166fcdfe14dd384f16c20382d3f4
Zach Daniel <zach@zachdaniel.dev>
2021-05-18 14:19:54 -0400

    chore: fix lint

--- test/support/resources/nested_enum.ex
+++ test/support/resources/nested_enum.ex
@@ -1,5 +1,6 @@
 defmodule AshGraphql.Test.NestedEnum do
+  @moduledoc false
   use Ash.Type.Enum, values: [:foo, :bar]
 
-  def graphql_type(), do: :nested_enum
+  def graphql_type, do: :nested_enum
 end
