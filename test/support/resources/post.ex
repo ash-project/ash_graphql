c7d1d804cecc8b8401e4d98513552c9c1273a59b
Michael St Clair <michaelst57@gmail.com>
2021-09-11 13:46:02 -0600

    failing test for root errors

--- test/support/resources/post.ex
+++ test/support/resources/post.ex
@@ -67,6 +67,7 @@ defmodule AshGraphql.Test.Post do
       create :create_post_with_comments_and_tags, :with_comments_and_tags
 
       update :update_post, :update
+      create :update_post_confirm, :update_confirm
       update :update_best_post, :update, read_action: :best_post, identity: false
 
       destroy :archive_post, :archive
@@ -139,7 +140,13 @@ defmodule AshGraphql.Test.Post do
       filter(expr(best == true))
     end
 
-    update(:update)
+    update :update, primary?: true
+
+    update :update_confirm do
+      argument(:confirmation, :string)
+      validate(confirm(:text, :confirmation))
+    end
+
     destroy(:destroy, primary?: true)
 
     destroy :archive do
