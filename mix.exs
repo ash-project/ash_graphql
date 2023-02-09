804390b4338941c529a3b6b61ce3e739ce7fd818
Zach Daniel <zach@zachdaniel.dev>
2023-02-09 01:13:32 -0500

    chore: don't depend on overly restrained version of ash

--- mix.exs
+++ mix.exs
@@ -137,7 +137,7 @@ defmodule AshGraphql.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ash, ash_version("~> 2.5.11")},
+      {:ash, ash_version("~> 2.5")},
       {:absinthe_plug, "~> 1.4"},
       {:absinthe, "~> 1.7"},
       {:dataloader, "~> 1.0"},
