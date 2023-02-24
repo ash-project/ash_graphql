e6aadc12210a23960fef656629fe07f1caaf9434
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2023-02-24 16:00:00 +0100

    fix: always return a list from pkey_fields (#59)

--- lib/resource/resource.ex
+++ lib/resource/resource.ex
@@ -2872,14 +2872,14 @@ defmodule AshGraphql.Resource do
         attribute = Ash.Resource.Info.attribute(resource, field)
         field_type = maybe_wrap_non_null(:id, require?)
 
-        %Absinthe.Blueprint.Schema.FieldDefinition{
+        [%Absinthe.Blueprint.Schema.FieldDefinition{
           description: attribute.description,
           identifier: field,
           module: schema,
           name: to_string(attribute.name),
           type: field_type,
           __reference__: ref(__ENV__)
-        }
+        }]
 
       fields ->
         for field <- fields do
