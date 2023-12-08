85ad7d1aed2e84ea9bab36f582d5835bde9afdff
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2023-12-08 11:59:32 +0100

    a step in the right direction?

--- test/support/pub_sub.ex
+++ test/support/pub_sub.ex
@@ -10,7 +10,7 @@ defmodule AshGraphql.Test.PubSub do
   end
 
   def subscribe(topic) do
-    IO.inspect([topic: topic], label: "subscribe")
+    # IO.inspect([topic: topic], label: "subscribe")
     Registry.register(__MODULE__, topic, [self()])
     :ok
   end
@@ -22,7 +22,8 @@ defmodule AshGraphql.Test.PubSub do
         event: "subscription:data",
         result: data
       }
-      |> IO.inspect(label: :publish_subscription)
+
+    # |> IO.inspect(label: :publish_subscription)
 
     Registry.dispatch(__MODULE__, topic, fn entries ->
       for {pid, _} <- entries, do: send(pid, {:broadcast, message})
@@ -30,7 +31,7 @@ defmodule AshGraphql.Test.PubSub do
   end
 
   def broadcast(topic, event, notification) do
-    IO.inspect([topic: topic, event: event, notification: notification], label: "broadcast")
+    # IO.inspect([topic: topic, event: event, notification: notification], label: "broadcast")
 
     message =
       %{
@@ -44,9 +45,16 @@ defmodule AshGraphql.Test.PubSub do
     end)
   end
 
-  def publish_mutation(_proxy_topic, _mutation_result, _subscribed_fields) do
+  def publish_mutation(proxy_topic, mutation_result, subscribed_fields) do
     # this pubsub is local and doesn't support clusters
     IO.inspect("publish mutation")
+
+    send(
+      Application.get_env(__MODULE__, :notifier_test_pid) |> IO.inspect(label: :send_to),
+      {:broadcast, proxy_topic, mutation_result, subscribed_fields}
+    )
+    |> IO.inspect(label: :send)
+
     :ok
   end
 end
