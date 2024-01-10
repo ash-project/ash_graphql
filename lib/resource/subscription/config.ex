18a028922d90674c0f6bd05b55ef17d062a647ec
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-01-10 20:02:15 +0100

    returned my first result from a subscription

--- lib/resource/subscription/config.ex
+++
@@ -1,9 +0,0 @@
-defmodule AshGraphql.Resource.Subscription.Config do
-  @callback config(args :: map(), info :: map()) :: {:ok, Keyword.t()} | {:error, Keyword.t()}
-
-  defmacro __using__(_) do
-    quote do
-      @behaviour AshGraphql.Resource.Subscription.Config
-    end
-  end
-end
