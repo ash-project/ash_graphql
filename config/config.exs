8f04ef2e1e4ce44f05c77b35b8fed672c9749b15
Zach Daniel <zach@zachdaniel.dev>
2024-05-13 12:43:13 -0400

    chore: set log level in dev

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,8 @@ config :ash, :disable_async?, true
 config :ash, :validate_domain_resource_inclusion?, false
 config :ash, :validate_domain_config_inclusion?, false
 
+config :logger, level: :warning
+
 if Mix.env() == :dev do
   config :git_ops,
     mix_project: AshGraphql.MixProject,
