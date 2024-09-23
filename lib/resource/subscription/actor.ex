e14d5911bcbe9ff7ca026c36f17002ad24d62c4e
Barnabas Jovanovics <barnabas.jovanovics@gmail.com>
2024-09-23 19:09:31 +0200

    format code

--- lib/resource/subscription/actor.ex
+++ lib/resource/subscription/actor.ex
@@ -1,5 +1,5 @@
 defmodule AshGraphql.Resource.Subscription.Actor do
-  # I'd like to have the typesp say that actor can be anything
+  # I'd like to have the typespec say that actor can be anything
   # but that the input and output must be the same
   @callback actor(actor :: any, opts :: Keyword.t()) :: actor :: any
 end
