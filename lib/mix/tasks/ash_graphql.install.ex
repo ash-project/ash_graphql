1abb422e34d97d01bdb97925c341fefc410e51e1
aifrak <aifrak@users.noreply.github.com>
2024-10-04 13:15:01 +0000

    docs: Use correct project name in moduledoc

--- lib/mix/tasks/ash_graphql.install.ex
+++ lib/mix/tasks/ash_graphql.install.ex
@@ -1,5 +1,5 @@
 defmodule Mix.Tasks.AshGraphql.Install do
-  @moduledoc "Installs AshGraphql. Should be run with `mix igniter.install ash_postgres`"
+  @moduledoc "Installs AshGraphql. Should be run with `mix igniter.install ash_graphql`"
   @shortdoc @moduledoc
   require Igniter.Code.Common
   use Igniter.Mix.Task
