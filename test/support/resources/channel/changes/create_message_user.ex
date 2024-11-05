8b624232282df62a4356cbc1ccc314fa149f7710
Junil Chon <jichon@gmail.com>
2024-11-05 13:19:46 -0600

    chore: provide a test for relationship filter by actor

---
+++ test/support/resources/channel/changes/create_message_user.ex
@@ -0,0 +1,24 @@
+defmodule AshGraphql.Test.Changes.CreateMessageUser do
+  @moduledoc false
+  use Ash.Resource.Change
+
+  def change(changeset, _, context) do
+    IO.inspect(context: context)
+
+    changeset
+    |> Ash.Changeset.after_action(fn _, result ->
+      case AshGraphql.Test.MessageUser
+           |> Ash.Changeset.for_create(:create, %{
+             message_id: changeset.data.id,
+             user_id: context.actor.id
+           })
+           |> Ash.create() do
+        {:ok, _} ->
+          {:ok, result}
+
+        {:error, error} ->
+          {:error, error}
+      end
+    end)
+  end
+end
