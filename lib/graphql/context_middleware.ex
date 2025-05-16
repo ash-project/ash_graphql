4df81a7d89519afb11c84ce4a45278550dc0e08c
marco Dell'Olio <dellolio.marco@gmail.com>
2025-05-15 21:58:23 -0400

    wip

--- lib/graphql/context_middleware.ex
+++ lib/graphql/context_middleware.ex
@@ -2,6 +2,9 @@ defmodule AshGraphql.Graphql.ContextMiddleware do
   @moduledoc false
 
   def extend_context(resolution, extend_context) do
+    IO.inspect(resolution)
+    IO.inspect(extend_context)
+
     Map.update!(resolution, :context, &Map.merge(&1, extend_context))
   end
 end
