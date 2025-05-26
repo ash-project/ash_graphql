3988820175cbbe011a3a307b1760fca4022e3d3e
Zach Daniel <zach@zachdaniel.dev>
2025-05-26 00:30:01 -0400

    chore: use string for regex

--- test/support/types/string_new_type.ex
+++ test/support/types/string_new_type.ex
@@ -1,4 +1,4 @@
 defmodule AshGraphql.Types.StringNewType do
   @moduledoc false
-  use Ash.Type.NewType, subtype_of: :string, constraints: [match: ~r/hello/]
+  use Ash.Type.NewType, subtype_of: :string, constraints: [match: "hello"]
 end
